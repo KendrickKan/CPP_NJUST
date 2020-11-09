bf182537f8e8883f0798d17fd0618d2c674191e4
KendrickKan <kandong.njust.edu.cn>
2020-11-09 18:58:53 +0800

    20201109

--- CPPHomework/unit10_18.cpp
+++ CPPHomework/unit10_18.cpp
@@ -1,4 +1,9 @@
-//ctrl alt i new-head
+/*
+ * @Author: KendrickKan 
+ * @Date: 2020-11-01 11:27:10 
+ * @Last Modified by: KendrickKan
+ * @Last Modified time: 2020-11-06 19:06:24
+ */
 #include <bits/stdc++.h>
 #define FastIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 #define rep(i, a, b) for (int i = a; i < b; i++)
@@ -17,9 +22,74 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
+enum Suit
+{
+    BigJoker = 1,
+    LittleJoker,
+    Heart,
+    Diamond,
+    Club,
+    Spade
+};
+class Card
+{
+public:
+    int dianshu;
+    Suit suit;
+    void print();
+};
+
+bool cmp(Card a, Card b)
+{
+    return rand() % 2 == 1 ? -1 : 1;
+}
+void Card::print()
+{
+    if (this->suit != 1 && this->suit != 2)
+        cout << this->suit << " " << this->dianshu << endl;
+    else
+        cout << this->suit << endl;
+}
+class Deck
+{
+public:
+    Card card[54];
+    Deck();
+    void print();
+    void shuffle();
+};
+Deck::Deck()
+{
+    card[0].suit = BigJoker;
+    card[0].dianshu = 0;
+    card[1].suit = LittleJoker;
+    card[1].dianshu = 0;
+    for (int i = 2; i < 54; i++)
+    {
+        card[i].dianshu = (i - 2) / 4 + 2;
+        if (((i - 2) % 4 + 1) == 1)
+            card[i].suit = Heart;
+        else if (((i - 2) % 4 + 1) == 2)
+            card[i].suit = Diamond;
+        if (((i - 2) % 4 + 1) == 3)
+            card[i].suit = Club;
+        if (((i - 2) % 4 + 1) == 4)
+            card[i].suit = Spade;
+    }
+}
+void Deck::print()
+{
+    for (int i = 0; i < 54; i++)
+        this->card[i].print();
+}
+
+void Deck::shuffle()
+{
+    sort(card, card + 54, cmp);
+}
+
 int main()
 {
     FastIO;
-    
     return 0;
 }
--- temptest/tempCodeRunnerFile.cpp
+++ temptest/tempCodeRunnerFile.cpp
@@ -1,55 +1,110 @@
+/*
+ * @Author: KendrickKan 
+ * @Date: 2020-11-09 16:32:50 
+ * @Last Modified by: KendrickKan
+ * @Last Modified time: 2020-11-09 17:21:29
+ */
 #include <bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define repp(i, a, b) for (int i = a; i <= b; i++)
+#define rep1(i, a, b) for (int i = a; i >= b; i--)
+#define mem0(a) memset(a, 0, sizeof(a))
+#define memfu1(a) memset(a, -1, sizeof(a))
+#define pb push_back
+#define fi first
+#define se second
+#define EPS 1e-10
+#define MOD 1e9 + 7
+#define PI acos(-1)
 using namespace std;
-class Date
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+int l, r, c;
+int beginx, beginy, beginz;
+int overx, overy, overz;
+struct node
 {
-    int year, month, day;
-
-public:
-    Date(int y, int m, int d) : year(y), month(m), day(d)
-    {
-        cout << "Constructorl of Date: ";
-        print();
-        cout << endl;
-    }
-    Date()
-    {
-        time_t ltime = time(NULL);
-        tm *today = localtime(&ltime);
-        year = today->tm_year + 1900;
-        month = today->tm_mon + 1;
-        day = today->tm_mday;
-        cout << "Default Constructorl of Date: ";
-        print();
-        cout << endl;
-    }
-    ~Date()
-    {
-        cout << "Destructor of Date: ";
-        print();
-        cout << endl;
-    }
-    int getYear() { return year; }
-    int getMonth() { return month; }
-    int getDay() { return day; }
-    void print()
-    {
-        cout << year << '.' << month << '.' << day;
-    }
+    int x, y, z;
+    int step;
 };
+char mp[55][55][55];
+bool vis[55][55][55];
+int dir[6][3] = {{0, 1, 0}, {0, -1, 0}, {1, 0, 0}, {-1, 0, 0}, {0, 0, 1}, {0, 0, -1}};
+bool check(int x, int y, int z)
+{
+    if (x < 0 || y < 0 || z < 0 || x >= l || y >= r || z >= c || mp[x][y][z] == '#' || vis[x][y][z] == 1)
+        return false;
+    return true;
+}
+int bfs()
+{
+    node nown, nextn;
+    nown.x = beginx;
+    nown.y = beginy;
+    nown.z = beginz;
+    vis[beginx][beginy][beginz] = 1;
+    nown.step = 0;
+    queue<node> q;
+    q.push(nown);
+    while (!q.empty())
+    {
+        nown = q.front();
+        q.pop();
+        if (mp[nown.x][nown.y][nown.z] == 'E')
+            return nown.step;
+        rep(i, 0, 6)
+        {
+            //int temp1 = dir[i][0];
+            //int temp2 = dir[i][1];
+            //int temp3 = dir[i][2];
+            nextn.x = nown.x + dir[i][0];
+            nextn.y = nown.y + dir[i][1];
+            nextn.z = nown.z + dir[i][2];
+            if (check(nextn.x, nextn.y, nextn.z))
+            {
+                vis[nextn.x][nextn.y][nextn.z] = 1;
+                nextn.step = nown.step + 1;
+                q.push(nextn);
+            }
+        }
+    }
+    return 0;
+}
 int main()
 {
-    Date date1(2002, 10, 1);
-    cout << "date1:";
-    date1.print();
-    cout << endl;
-    Date *date2 = new Date;
-    cout << "date2:";
-    date2->print();
-    cout << endl;
-    delete date2;
-    date1 = Date(2001, 12, 21);
-    cout << "date1:";
-    date1.print();
-    cout << endl;
+    FastIO;
+    while (cin >> l >> r >> c && (l || r || c))
+    {
+        rep(i, 0, l)
+        {
+            rep(j, 0, r)
+            {
+                rep(k, 0, c)
+                {
+                    cin >> mp[i][j][k];
+                    if (mp[i][j][k] == 'S')
+                    {
+                        beginx = i;
+                        beginy = j;
+                        beginz = k;
+                    }
+                    if (mp[i][j][k] == 'E')
+                    {
+                        overx = i;
+                        overy = j;
+                        overz = k;
+                    }
+                }
+            }
+        }
+        mem0(vis);
+        int ans = bfs();
+        if (ans)
+            cout << "Escaped in " << ans << " minute(s)." << endl;
+        else
+            cout << "Trapped!" << endl;
+    }
     return 0;
 }
