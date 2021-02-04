2400f46dee45d0ca98ff71518959cfb0b8825937
KendrickKan <kandong@njust.edu.cn>
2021-02-05 00:11:25 +0800

    20210204

---
+++ codelover/speedhand3/a.cpp
@@ -0,0 +1,55 @@
+/*
+ * @Author: KendrickKan 
+ * @Date: 2021-02-04 18:59:37 
+ * @Last Modified by: KendrickKan
+ * @Last Modified time: 2021-02-04 19:01:58
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define FastIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define repp(i, a, b) for (int i = a; i <= b; i++)
+#define rep1(i, a, b) for (int i = a; i >= b; i--)
+#define mem0(a) memset(a, 0, sizeof(a))
+#define memfu1(a) memset(a, -1, sizeof(a))
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define EPS 1e-10
+const int MOD = 1e9 + 7;
+//INT_MAX <limits>
+#define PI acos(-1)
+template <typename T>
+void read(T &t)
+{
+    t = 0;
+    char ch = getchar();
+    int f = 1;
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    do
+    {
+        (t *= 10) += ch - '0';
+        ch = getchar();
+    } while ('0' <= ch && ch <= '9');
+    t *= f;
+}
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+int main()
+{
+    FastIO;
+    int n;
+    cin >> n;
+    if (n % 2 == 0 && n != 2)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
