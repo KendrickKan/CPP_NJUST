9b27348bb8c97b546c76e8459f65159e22d503da
KendrickKan <kandong@njust.edu.cn>
2021-03-19 11:51:47 +0800

    heap

--- Algorithms Design and Analysis/chapter2/Heap.cpp
+++ Algorithms Design and Analysis/chapter2/Heap.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 const int maxnum = 1000;
 int heap[maxnum];
+int n;
 void Sift_up(int i)
 {
     bool done = false;
@@ -30,6 +31,24 @@ void Sift_up(int i)
 }
 void Sift_down(int i)
 {
+    bool down = false;
+    if (2 * i > n)
+        return;
+    while (2 * i <= n && !down)
+    {
+        i = 2 * i;
+        if (i + 1 <= n && heap[i + 1] > heap[i])
+            i = i + 1;
+        int tempson = (int)floor(i * 1.0 / 2);
+        if (heap[tempson] < heap[i])
+        {
+            int temp = heap[tempson];
+            heap[tempson] = heap[i];
+            heap[i] = temp;
+        }
+        else
+            down = true;
+    }
 }
 int main()
 {
