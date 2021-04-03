00a272ac394e97f7d2857688f8f245af3897c1ae
KendrickKan <kandong@njust.edu.cn>
2021-04-03 21:13:00 +0800

    round711div2

---
+++ codeforces/round711div2/a.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FastIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define fo(x) for (register int i = 1; i <= x; ++i)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define repp(i, a, b) for (int i = a; i <= b; i++)
+#define rep1(i, a, b) for (int i = a; i >= b; i--)
+#define mem0(a) memset(a, 0, sizeof(a))
+#define memfu1(a) memset(a, -1, sizeof(a))
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define EPS 1e-10
+const int MOD = 1e9 + 7;
+//INT_MAX <limits>
+#define PI acos(-1)
+template <typename T>
+void read(T &t)
+{
+    t = 0;
+    char ch = getchar();
+    int f = 1;
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    do
+    {
+        (t *= 10) += ch - '0';
+        ch = getchar();
+    } while ('0' <= ch && ch <= '9');
+    t *= f;
+}
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ll gcd(ll x, ll y)
+{
+    return y == 0 ? x : gcd(y, x % y);
+}
+int getsum(ll x)
+{
+    int sum = 0;
+    while (x > 0)
+    {
+        sum += x % 10;
+        x /= 10;
+    }
+    return sum;
+}
+int main()
+{
+    FastIO;
+    int t;
+    read(t);
+    while (t--)
+    {
+        ll n;
+        read(n);
+        while (gcd(n, getsum(n)) == 1)
+        {
+            n++;
+        }
+        cout << n << endl;
+    }
+    return 0;
+}
