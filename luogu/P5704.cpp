4786e81484e41d0813c01eb05c2c67c876ad9d5a
KendrickKan <kandong@njust.edu.cn>
2021-07-27 23:12:26 +0800

    luogu7_27

---
+++ luogu/P5704.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FastIO ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define fo(x) for (register int i = 1; i <= x; ++i)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define repp(i, a, b) for (int i = a; i <= b; i++)
+#define rep1(i, a, b) for (int i = a; i >= b; i--)
+#define mem0(a) memset(a, 0, sizeof(a))
+#define memfu1(a) memset(a, -1, sizeof(a))
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define EPS 1e-10
+const int MOD = 1e9 + 7;
+//INT_MAX <limits>
+#define PI acos(-1)
+template <typename T>
+void read(T &t)
+{
+    t = 0;
+    char ch = getchar();
+    int f = 1;
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    do
+    {
+        (t *= 10) += ch - '0';
+        ch = getchar();
+    } while ('0' <= ch && ch <= '9');
+    t *= f;
+}
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+int main()
+{
+    FastIO;
+    int s, v;
+    cin >> s >> v;
+    int totalMin = 10 + (int)ceil(s * 1.0 / v);
+    int h = totalMin / 60 + 1;
+    int m = totalMin % 60;
+    if (h < 8)
+    {
+        cout << 0 << 8 - h;
+    }
+    else if (h == 8)
+        cout << 0 << 0;
+    else
+    {
+        h -= 8;
+        if (24 - h < 10)
+            cout << 0 << 24 - h;
+        else
+            cout << 24 - h;
+    }
+    cout << ":";
+    if (m == 0)
+        cout << 0 << 0;
+    else if (m > 50)
+        cout << 0 << 60 - m;
+    else
+        cout << 60 - m;
+    return 0;
+}
