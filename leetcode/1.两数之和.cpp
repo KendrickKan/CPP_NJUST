95f057cffeaa635b9a57c38a320cf0d695df82fb
KendrickKan <kandong@njust.edu.cn>
2022-08-25 10:42:50 +0800

    leetcode 两数之和

---
+++ leetcode/1.两数之和.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size();i++){
+            for(int j = 0; j < nums.size();j++){
+                if(j==i)
+                    continue;
+                if(nums[j]+nums[i]==target)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
