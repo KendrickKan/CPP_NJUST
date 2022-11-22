d7915f78c0b30405d543bc416fcc1d8724d98a94
KendrickKan <kandong@njust.edu.cn>
2022-11-23 00:55:27 +0800

    动手学深度学习

---
+++ Dive Into Deep Learning/lr.py
@@ -0,0 +1,20 @@
+import random
+import torch
+from d2l import torch as d2l
+
+
+def synthetic_data(w, b, num_examples):  #@save
+    """生成y=Xw+b+噪声"""
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.01, y.shape)
+    return X, y.reshape((-1, 1))
+
+
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+features, labels = synthetic_data(true_w, true_b, 1000)
+print('features:', features[0], '\nlabel:', labels[0])
+d2l.set_figsize()
+d2l.plt.scatter(features[:, (1)].detach().numpy(), labels.detach().numpy(), 1)
+d2l.plt.show()
