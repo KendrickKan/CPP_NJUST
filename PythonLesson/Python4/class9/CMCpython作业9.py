6d6d2b2cefb8042272b415f0acc522ae956f948d
KendrickKan <kandong@njust.edu.cn>
2022-12-05 16:48:24 +0800

    PythonClass10

---
+++ PythonLesson/Python4/class9/CMCpython作业9.py
@@ -0,0 +1,18 @@
+def he(l,k):
+        return   l+k
+print(he(646457,33))
+def dfg(klj,uy):
+        return (klj*uy)/2
+print(float(dfg(998888776.234544322344,988888888888)))
+def sdnb(gg,hhhhhhh):
+        return gg*gg*hhhhhhh
+print(float(sdnb(3444,3.1415926)))
+def jinitaimet(a):
+        g=1
+        for  jinitaimet in range(1,a):
+                if jinitaimet%2==0:
+                        g +=jinitaimet
+        if a % 2 == 0:
+                g = g + a
+        return g-1
+print(jinitaimet(6666666666666666666666))
