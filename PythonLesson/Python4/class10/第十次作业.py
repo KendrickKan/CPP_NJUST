fc33faea59d375044e2a46dc04fddd8e623b3c39
KendrickKan <kandong@njust.edu.cn>
2022-12-09 20:20:53 +0800

    python class 11

---
+++ PythonLesson/Python4/class10/第十次作业.py
@@ -0,0 +1,59 @@
+import math
+from math import pi
+def myd(d):
+    return pi*d*d
+print(myd(5))
+
+
+
+def myg(r):
+    return 4/3*pi*r*r
+print(myg(6))
+
+
+
+dtr = "Hello"
+str = "Python"
+lih = list(dtr)
+lij = list(str)
+print(lih,lij)
+lih.extend(lij)
+print(lih)
+lih.append('!')
+print(lih)
+print(str[0])
+print(str[-5])
+print(str[4])
+print(str[-1])
+print(str[2:6])
+print(str[-4:])
+print(str[3:])
+print(str[-3:])
+
+print(lih[0])
+print(lih[-12])
+print(lih[11])
+print(lih[-1])
+print(lih[2:6])
+
+#1 D
+#2 B
+#3 C
+#4 D
+#5 B
+#6 B
+#7 A
+#8 B
+#9 C
+#10 B
+#11 D
+#12 没学
+#13 A 
+#14 c
+#15 D
+#16 CD
+#17 ACD
+#18 ABD
+#19 AD
+
+
