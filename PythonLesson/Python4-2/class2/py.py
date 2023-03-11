3119203f5134d6f2e274be6bbd0ba56efb9c1fe3
KendrickKan <kandong@njust.edu.cn>
2023-03-11 16:51:55 +0800

    Python Class

---
+++ PythonLesson/Python4-2/class2/py.py
@@ -0,0 +1,21 @@
+a=9
+b=4
+c=3.14
+print(type(a))
+print(type(c))
+print(float(a))
+print (float(b))
+print(int(c))
+print(a+b*a)
+print(int(a+b+c))
+# int 整数类型
+# float 浮点数类型
+# str 字符串类型 str的全称是string
+
+# type()函数可以查看变量的类型 括号里面放变量
+
+# 整数类型和浮点数类型可以相互转换
+# int()函数可以将浮点数转换成整数 但是会舍去小数部分 只保留整数部分
+# float()函数可以将整数转换成浮点数 但是会在整数后面加上小数点和0
+
+# print()函数可以打印输出变量的值 括号里面放变量
